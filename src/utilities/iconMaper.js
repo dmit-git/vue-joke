72499415508aee52e48668b706a6a7a6f772e37d
dmit-git <firdmitryi.work@gmail.com>
2018-02-15 10:28:34 +0100

    Reformat icon mapper.

--- src/utilities/iconMaper.js
+++ src/utilities/iconMaper.js
@@ -2,52 +2,52 @@ export default {
   getIconString: name => {
     switch (name) {
       case "explicit":
-            return 'supervisor_account';
+        return "supervisor_account";
         break;
       case "dev":
-            return 'gavel';
+        return "gavel";
         break;
       case "movie":
-            return 'movie';
+        return "movie";
         break;
       case "food":
-            return 'restaurant';
+        return "restaurant";
         break;
       case "celebrity":
-            return 'face';
+        return "face";
         break;
       case "science":
-            return 'developer_board';
+        return "developer_board";
         break;
       case "sport":
-      return 'directions_bike';
+        return "directions_bike";
         break;
       case "political":
-      return 'account_balance';
+        return "account_balance";
         break;
       case "religion":
-      return 'remove_red_eye';
+        return "remove_red_eye";
         break;
       case "animal":
-        return 'pets';
+        return "pets";
         break;
       case "history":
-      return 'receipt';
+        return "receipt";
         break;
       case "music":
-      return 'music_note';
+        return "music_note";
         break;
       case "travel":
-      return 'airplanemode_active';
+        return "airplanemode_active";
         break;
       case "career":
-      return 'trending_up';
+        return "trending_up";
         break;
       case "money":
-      return 'attach_money';
+        return "attach_money";
         break;
       case "fashion":
-      return 'wc';
+        return "wc";
         break;
 
       default:
